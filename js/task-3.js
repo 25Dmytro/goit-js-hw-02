2be9a76054c7132a40ec1280eef2a1ca7d765f29
25Dmytro <25kapkan@gmail.com>
2024-03-05 21:33:39 +0200

    final1

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,13 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const totalSize = contentWidth + paddingWidth * 2 + borderWidth * 2;
-    return totalSize;
+function checkForSpam(message) {
+    const messagelowerCase = message.toLowerCase();
+    return messagelowerCase.includes("spam") || messagelowerCase.includes("sale");
 }
 
 //Перевірка
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
